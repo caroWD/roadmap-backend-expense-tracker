c0243bee1722fe53c3023b224daa13b222871378
caroWD <juego.fuego@gmail.com>
2025-12-22 18:04:39 -0500

    feat: category: domain: add and implement created at value object

--- src/lib/category/domain/Category.ts
+++ src/lib/category/domain/Category.ts
@@ -1,13 +1,25 @@
-import { CategoryDescription, CategoryId, CategoryName } from './object_values/index.js'
+import {
+  CategoryDescription,
+  CategoryId,
+  CategoryName,
+  CategoryCreatedAt,
+} from './object_values/index.js'
 
 export class Category {
   id: CategoryId
   name: CategoryName
   description: CategoryDescription
+  createdAt: CategoryCreatedAt
 
-  constructor(id: CategoryId, name: CategoryName, description: CategoryDescription) {
+  constructor(
+    id: CategoryId,
+    name: CategoryName,
+    description: CategoryDescription,
+    createdAt: CategoryCreatedAt,
+  ) {
     this.id = id
     this.name = name
     this.description = description
+    this.createdAt = createdAt
   }
 }
--- src/lib/category/domain/object_values/index.ts
+++ src/lib/category/domain/object_values/index.ts
@@ -1,3 +1,4 @@
 export { CategoryId } from './CategoryId.js'
 export { CategoryName } from './CategoryName.js'
 export { CategoryDescription } from './CategoryDescription.js'
+export { CategoryCreatedAt } from './CategoryCreatedAt.js'
