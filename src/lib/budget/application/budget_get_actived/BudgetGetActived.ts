31ed33da5d3d91bd21da5fde268df7f67760eecb
caroWD <juego.fuego@gmail.com>
2025-12-29 20:16:08 -0500

    fix: budget: application: get actived use case: type return fixed

--- src/lib/budget/application/budget_get_actived/BudgetGetActived.ts
+++ src/lib/budget/application/budget_get_actived/BudgetGetActived.ts
@@ -1,10 +1,5 @@
 import type { Budget } from '../../domain/Budget.js'
-import { BudgetDto } from '../../domain/BudgetDto.js'
 import type { IBudgetRepository } from '../../domain/interfaces/IBudgetRepository.js'
-import { BudgetDtoAmount } from '../../domain/object_values/BudgetDtoAmount.js'
-import { BudgetDtoCreatedAt } from '../../domain/object_values/BudgetDtoCreatedAt.js'
-import { BudgetDtoId } from '../../domain/object_values/BudgetDtoId.js'
-import { BudgetDtoState } from '../../domain/object_values/BudgetDtoState.js'
 
 export class BudgetGetActived {
   private readonly _budgetRepository: IBudgetRepository
@@ -13,16 +8,11 @@ export class BudgetGetActived {
     this._budgetRepository = budgetRepository
   }
 
-  async Run(): Promise<BudgetDto> {
+  async Run(): Promise<number> {
     const budgetActived: Budget | null = await this._budgetRepository.GetActived()
 
-    if (!budgetActived) throw new Error('No budget has been defined.')
+    if (!budgetActived) return 0
 
-    return new BudgetDto(
-      new BudgetDtoId(budgetActived.id.value),
-      new BudgetDtoAmount(budgetActived.amount.value),
-      new BudgetDtoState(budgetActived.state.value === 1 ? 'actived' : 'desactived'),
-      new BudgetDtoCreatedAt(budgetActived.createdAt.value.toLocaleDateString('en-CA')),
-    )
+    return budgetActived.amount.value
   }
 }
